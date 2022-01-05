54881bc8a05652fa4a8680b341e84bb84295c982
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2022-01-05 14:20:01 +0100

    responsivitet

--- script.js
+++ script.js
@@ -96,43 +96,67 @@ function aboutEducationList() {
     // H2 Medieinstitutet
     const educationH2Mi = document.createElement("h2");
     educationH2Mi.className = "h2Education";
-    educationH2Mi.innerText = "Medieinstitutet (2021 - 2023)";
+    educationH2Mi.innerHTML = "Medieinstitutet (&nbsp;2021&nbsp;-&nbsp;2023&nbsp;)";
+
+    // H3 Medieinstitutet
+    const educationH3Mi = document.createElement("h3");
+    educationH3Mi.innerText = "Front End Developer";
 
     // P Medieinstitutet
     const educationPMi = document.createElement("p");
-    educationPMi.innerText = "Front End Developer";
+    educationPMi.innerHTML = "<b>Utbildningen inkluderar främst:</b><br> HTML, CSS (Sass), JavaScript <br><br> <b>Ramverk:</b><br> React <br><br><b>Bibliotek:</b><br> jQuery <br><br><b>Övrigt:</b><br> Användbarhet, tillgänglighet, UX, API:er, projekt- och arbetsmetodik för utvecklare";
 
     // Div Södertörns högskola
     const divSH = document.createElement("div");
     divSH.className = "divEducation";
+    divSH.id = "divSH";
 
     // H2 Södertörns högskola
     const educationH2Sh = document.createElement("h2");
     educationH2Sh.className = "h2Education";
-    educationH2Sh.innerText = "Södertörns högskola (2012 - 2015)";
+    educationH2Sh.innerHTML = "Södertörns högskola (&nbsp;2012&nbsp;-&nbsp;2015&nbsp;)";
 
+    // H3 Södertörns högskola
+    const educationH3Sh = document.createElement("h3");
+    educationH3Sh.innerText = "Internationella ekonomprogrammet";
+    
     // P Södertörns högskola
     const educationPSh = document.createElement("p");
-    educationPSh.innerText = "Internationella ekonomprogrammet";
+    educationPSh.innerHTML = "<b>Utbildningen inkluderar:</b><br> Företagsekonomi (A, B och C) <br><br> Statisik för ekonomer <br><br> Engelska A <br><br> Nationalekonomi A <br><br> Handelsrättslig översiktskurs<br> ";
+
+    // Div övrigt utbildning
+    const divDiverse = document.createElement("div");
+    divDiverse.className = "divEducation";
+    divDiverse.id = "divDiverse";
+   
+    // H2 övrigt utbildning
+    const educationH2Diverse = document.createElement("h2");
+    educationH2Diverse.innerText = "Övrigt"
+   
+    // P övrigt utbildning
+    const educationPDiverse = document.createElement("p");
+    educationPDiverse.innerHTML= "<b>Code Institute</b><br> 5 Day Code Challenge <br><br> <b>Tjejer kodar</b><br> 4-dagars kurs"
 
     // Div Jensen gymnasium Södra
     const divJensen = document.createElement("div");
     divJensen.className = "divEducation";
+    divJensen.id = "divJensen";
 
     // H2 Jensen gymnasium Södra
     const educationH2Jgs = document.createElement("h2");
-    educationH2Jgs.innerText = "Jensen Gymnasium Södra (2007 - 2010)"
+    educationH2Jgs.innerHTML = "Jensen Gymnasium Södra <wbr> (&nbsp;2007&nbsp;-&nbsp;2010&nbsp;)"
 
     // P Jensen gymnasium Södra
     const educationPJgs = document.createElement("p");
     educationPJgs.innerText = "Företagande och försäljning"
 
-
     // Appenda nya element
-    sectionContent.append(educationH1, divMedieinstitutet, divSH, divJensen);
-    divMedieinstitutet.append(educationH2Mi, educationPMi);
-    divSH.append(educationH2Sh, educationPSh);
-    divJensen.append(educationH2Jgs, educationPJgs)
+    sectionContent.append(educationH1, divMedieinstitutet, divSH, divDiverse, divJensen);
+    divMedieinstitutet.append(educationH2Mi, educationH3Mi, educationPMi);
+    divSH.append(educationH2Sh, educationH3Sh, educationPSh);
+    divDiverse.append(educationH2Diverse, educationPDiverse);
+    divJensen.append(educationH2Jgs, educationPJgs);
+    
 }
 
 
