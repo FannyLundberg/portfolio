694dfecdbe317f1130fbaaebfc7d62290838a0f1
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2022-01-10 12:34:12 +0100

    WIP

--- modules/homepage.mjs
+++ modules/homepage.mjs
@@ -23,31 +23,31 @@ export function homePage() {
     const homeH3 = document.createElement("h3");
 
     
-    //Nedräkning till examensdatum
+    // Nedräkning till examensdatum
     const examensDatum = new Date("Juni 9, 2023 12:00:00").getTime();
 
     const x = setInterval(function() {
 
         // Hämta dagens datum
-        let dagensDatum = new Date().getTime();
+        let dateToday = new Date().getTime();
     
         // Skillnad mellan dagens datum och examensdatum
-        let countDown = examensDatum - dagensDatum;
+        let countDown = examensDatum - dateToday;
     
         // Räkna fram dagar, timmar, minuter och sekunder
-        let dagar = Math.floor(countDown / (1000 * 60 * 60 * 24));
-        let timmar = Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minuter = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
-        let sekunder = Math.floor((countDown % (1000 * 60)) / 1000);
+        let days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((countDown % (1000 * 60)) / 1000);
     
         // Visa nedräkning i paragrafen
-        homeH3.innerHTML = "Examen om: " + dagar + "d " + timmar + "h " + minuter + "m " + sekunder + "s ";
+        homeH3.innerHTML = "Examen om: " + days + "d " + "  " + hours + "h " + "  " + minutes + "m " + "  " + seconds + "s ";
 
     }, 1000);
 
-    // H3 till första sidan
+    // H4 till första sidan
     const homeH4 = document.createElement("h4");
-    homeH4.innerHTML = "En strukturerad, lösningsorienterad och positiv <br> person som nu kommit på vad jag ska bli när jag blir stor";
+    homeH4.innerHTML = 'En strukturerad, lösningsorienterad och positiv <br> person som nu kommit på "vad jag ska bli när jag blir stor"';
 
     // Appenda nya elementen
     sectionContent.append(profileImgHome, homeH1, homeH2, homeH3, homeH4);
