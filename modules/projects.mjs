ea9b7ddaa401a860391c829b93eba7b1a22f4d4b
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2022-01-14 10:34:40 +0100

    Styling länkar i Projekt-fliken

--- modules/projects.mjs
+++ modules/projects.mjs
@@ -12,16 +12,19 @@ export function aboutProjectList() {
     const projectA1 = document.createElement("a");
     const linkText1 = 'Repo "Lunchrestaurang med switch"'
     projectA1.href = "https://github.com/FannyLundberg/lunch.git";
+    projectA1.className = "projectLink";
 
     // A - repo ålder
     const projectA2 = document.createElement("a");
     const linkText2 = 'Repo "Är du tillräckligt gammal?"'
     projectA2.href = "https://github.com/FannyLundberg/Are-you-old-enough.git";
+    projectA2.className = "projectLink";
 
     // A - repo strängpoesi
     const projectA3 = document.createElement("a");
     const linkText3 = 'Repo "Strängpoesi"'
     projectA3.href = "https://github.com/FannyLundberg/word-poetry.git";
+    projectA3.className = "projectLink";
 
     // Appenda nya element
     sectionContent.append(projectH1, projectA1, projectA2, projectA3);
