8ecb1a8bd8c4ab5cbcdf3c23c55f1f8af12fe31e
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2022-01-15 22:51:59 +0100

    lagt till länk till projekt Stelin entreprenad

--- modules/projects.mjs
+++ modules/projects.mjs
@@ -26,9 +26,16 @@ export function aboutProjectList() {
     projectA3.href = "https://github.com/FannyLundberg/word-poetry.git";
     projectA3.className = "projectLink";
 
+    // Stelin Entreprenad
+    const projectA4 = document.createElement("a");
+    const linkText4 = 'Hemsida till Stelin Entreprenad'
+    projectA4.href = "https://stelinentreprenad.se/";
+    projectA4.className = "projectLink";
+
     // Appenda nya element
-    sectionContent.append(projectH1, projectA1, projectA2, projectA3);
+    sectionContent.append(projectH1, projectA1, projectA2, projectA3, projectA4);
     projectA1.append(linkText1);
     projectA2.append(linkText2);
     projectA3.append(linkText3);
+    projectA4.append(linkText4);
 }
