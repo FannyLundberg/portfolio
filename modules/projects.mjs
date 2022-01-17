dc4233c981ab7eaa7b5fc45804c11fc820b7bc52
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2022-01-17 14:51:53 +0100

    uppdaterat länkar för projekt, GitHub Pages

--- modules/projects.mjs
+++ modules/projects.mjs
@@ -10,25 +10,25 @@ export function aboutProjectList() {
 
     // A - repo Lunch
     const projectA1 = document.createElement("a");
-    const linkText1 = 'Repo "Lunchrestaurang med switch"'
+    const linkText1 = '"Lunchrestaurang med switch"'
     projectA1.href = "https://fannylundberg.github.io/lunch/";
     projectA1.className = "projectLink";
 
     // A - repo ålder
     const projectA2 = document.createElement("a");
-    const linkText2 = 'Repo "Är du tillräckligt gammal?"'
-    projectA2.href = "https://github.com/FannyLundberg/Are-you-old-enough.git";
+    const linkText2 = '"Att göra"-lista'
+    projectA2.href = "https://fannylundberg.github.io/ToDoList-clock-and-change-bg-color/";
     projectA2.className = "projectLink";
 
     // A - repo strängpoesi
     const projectA3 = document.createElement("a");
-    const linkText3 = 'Repo "Strängpoesi"'
-    projectA3.href = "https://github.com/FannyLundberg/word-poetry.git";
+    const linkText3 = '"Strängpoesi"'
+    projectA3.href = "https://fannylundberg.github.io/word-poetry/";
     projectA3.className = "projectLink";
 
     // Stelin Entreprenad
     const projectA4 = document.createElement("a");
-    const linkText4 = 'Hemsida till Stelin Entreprenad'
+    const linkText4 = 'Hemsida Stelin Entreprenad'
     projectA4.href = "https://stelinentreprenad.se/";
     projectA4.className = "projectLink";
 
