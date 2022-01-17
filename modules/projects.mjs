ee18d4dfe6e72207d02d93fefe4b29451fb9b2a0
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2022-01-17 15:11:22 +0100

    kommenterade ut ett projekt

--- modules/projects.mjs
+++ modules/projects.mjs
@@ -14,7 +14,7 @@ export function aboutProjectList() {
     projectA1.href = "https://fannylundberg.github.io/lunch/";
     projectA1.className = "projectLink";
 
-    // A - repo ålder
+    // A - "Att göra"-lista, klocka, datum samt ändra bakgrundsfärg och textfärg
     const projectA2 = document.createElement("a");
     const linkText2 = '"Att göra"-lista'
     projectA2.href = "https://fannylundberg.github.io/ToDoList-clock-and-change-bg-color/";
@@ -26,11 +26,13 @@ export function aboutProjectList() {
     projectA3.href = "https://fannylundberg.github.io/word-poetry/";
     projectA3.className = "projectLink";
 
+    /*
     // Stelin Entreprenad
     const projectA4 = document.createElement("a");
     const linkText4 = 'Hemsida Stelin Entreprenad'
     projectA4.href = "https://stelinentreprenad.se/";
     projectA4.className = "projectLink";
+    */
 
     // Appenda nya element
     sectionContent.append(projectH1, projectA1, projectA2, projectA3, projectA4);
