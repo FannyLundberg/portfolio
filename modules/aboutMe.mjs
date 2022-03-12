573e81f9762e0858bb0ec03913b9cd18ca5eaa68
Fanny Lundberg <93767753+FannyLundberg@users.noreply.github.com>
2022-03-12 15:27:53 +0100

    Uppdatering av Om mig-sidan

--- modules/aboutMe.mjs
+++ modules/aboutMe.mjs
@@ -7,11 +7,6 @@ export function aboutMeList() {
     // Profilbild
     const divAboutMe = document.createElement("div");
     divAboutMe.id = "divAboutMe";
-    
-    // Profilbild
-    const profileImgAboutMe = document.createElement("img");
-    profileImgAboutMe.src = "./img/profilbild2.jpg";
-    profileImgAboutMe.id = "profileImgAboutMe";
 
     // H1
     const aboutMeH1 = document.createElement("h1");
@@ -20,7 +15,7 @@ export function aboutMeList() {
 
     // H2
     const aboutMeH2 = document.createElement("h2");
-    aboutMeH2.innerText = "Provade på programmering och fastnade direkt."
+    aboutMeH2.innerText = "Jag älskar att koda!"
 
     // P Koda
     const aboutMeP1 = document.createElement("p");
@@ -28,9 +23,9 @@ export function aboutMeList() {
     
     // P Fritid
     const aboutMeP2 = document.createElement("p");
-    aboutMeP2.innerText = 'Utöver att koda tycker jag om att vara med mina nära och kära, lyssna på musik, podd och se dokumentärer. När världen "tillåter" tycker jag även om att resa och gå på konsert.'
+    aboutMeP2.innerText = 'Utöver att koda tycker jag om att vara med mina nära och kära, inredning, se dokumentärer, lyssna på musik och poddar. Jag tycker även om att resa och gå på konsert.'
 
     // Appenda nya element
     sectionContent.append(divAboutMe);
-    divAboutMe.append(profileImgAboutMe, aboutMeH1, aboutMeH2, aboutMeP1, aboutMeP2);
+    divAboutMe.append(aboutMeH1, aboutMeH2, aboutMeP1, aboutMeP2);
 }
